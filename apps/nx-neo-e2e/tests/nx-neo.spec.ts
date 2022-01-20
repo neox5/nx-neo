1aadfe747bb7044bb16a90c4858b586a4e4c5c88
neox5 <faustmannchr@gmail.com>
2022-01-20 21:23:12 +0100

    Fixes formating

--- apps/nx-neo-e2e/tests/nx-neo.spec.ts
+++ apps/nx-neo-e2e/tests/nx-neo.spec.ts
@@ -1,4 +1,3 @@
-import { logger } from "@nrwl/devkit";
 import {
   checkFilesExist,
   ensureNxProject,
@@ -18,10 +17,10 @@ describe("nx-neo e2e", () => {
     expect(true).toEqual(true);
   }, 65000);
 
-  describe('--directory', () => {
-    it('should create src in the specified directory', async () => {
-      const plugin = uniq('nx-neo');
-      ensureNxProject('@nx-neo/nx-neo', 'dist/libs/nx-neo');
+  describe("--directory", () => {
+    it("should create src in the specified directory", async () => {
+      const plugin = uniq("nx-neo");
+      ensureNxProject("@nx-neo/nx-neo", "dist/libs/nx-neo");
       await runNxCommandAsync(
         `generate @nx-neo/nx-neo:nx-neo ${plugin} --directory subdir`
       );
@@ -31,15 +30,15 @@ describe("nx-neo e2e", () => {
     }, 12000);
   });
 
-  describe('--tags', () => {
-    it('should add tags to the project', async () => {
-      const plugin = uniq('nx-neo');
-      ensureNxProject('@nx-neo/nx-neo', 'dist/libs/nx-neo');
+  describe("--tags", () => {
+    it("should add tags to the project", async () => {
+      const plugin = uniq("nx-neo");
+      ensureNxProject("@nx-neo/nx-neo", "dist/libs/nx-neo");
       await runNxCommandAsync(
         `generate @nx-neo/nx-neo:nx-neo ${plugin} --tags e2etag,e2ePackage`
       );
       const project = readJson(`libs/${plugin}/project.json`);
-      expect(project.tags).toEqual(['e2etag', 'e2ePackage']);
+      expect(project.tags).toEqual(["e2etag", "e2ePackage"]);
     }, 12000);
   });
 });
